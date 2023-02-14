68f632f4cc2d726aaf95e3f9ea1990683549a5a3
TI - Gabriel Goncalves <gabrielleiteadm@gmail.com>
2023-02-14 10:42:07 -0300

    patch

--- projeto-DOM/projeto_inicial/index.js
+++ projeto-DOM/projeto_inicial/index.js
@@ -1,7 +1,15 @@
-const novaTarefa = document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener('click', () => {
+const task = document.querySelector('[data-task]');
+
+const createTask = (event) => {
+  event.preventDefault()
+
   const input = document.querySelector('[data-form-input]')
-  const valor = input.value
-  console.log(valor)
-})
+  const content = input.value
+  console.log(content)
+  input.value = ''
+};
+
+const newTask = document.querySelector('[data-form-button]');
+
+newTask.addEventListener('click', createTask());
